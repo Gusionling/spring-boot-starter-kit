15c2cf7746de33326ba3dd1dfe5f9535682ab95d
Gusionling <isdnhk@naver.com>
2025-03-21 17:28:30 +0900

    [feat] swagger setting

--- src/main/java/com/hyeongkyu/template/global/constants/Constants.java
+++ src/main/java/com/hyeongkyu/template/global/constants/Constants.java
@@ -13,6 +13,8 @@ public class Constants {
     public static final String API_PREFIX = "/api/v1";
 
     public static final List<String> NO_NEED_AUTH_URLS = List.of(
+            "/v3/api-docs.html/**",
+            "/v3/api-docs/**",
             "/swagger-ui/**",
             "/api/v1/auth/**"
     );
